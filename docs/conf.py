d785a15ec1571f7fbe62066cc2913ea113a7632d
Marco Lancini <marco.lancini.ml@gmail.com>
2012-11-11 17:33:46 +0100

    docs

--- docs/conf.py
+++ docs/conf.py
@@ -16,14 +16,21 @@ import sys, os
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-#sys.path.insert(0, os.path.abspath('.'))
-BASE_PATH = os.path.abspath(os.path.dirname(__file__) + '/../')
-sys.path.append(BASE_PATH)
+# BASE_PATH = os.path.abspath(os.path.dirname(__file__) + '/../')
+# sys.path.append(BASE_PATH)
 
-import settings
-from django.core.management import setup_environ
-setup_environ(settings)
+# import settings
+# from django.core.management import setup_environ
+# setup_environ(settings)
 
+
+sys.path.append(os.path.dirname(__file__))
+import django 
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
+if django.VERSION < (1, 4):
+    from django.core.management import setup_environ
+    settings = __import__(os.environ["DJANGO_SETTINGS_MODULE"])
+    setup_environ(settings)
 
 
 # -- General configuration -----------------------------------------------------
